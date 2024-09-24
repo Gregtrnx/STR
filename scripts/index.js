051615d807804b34cfb29e5f44994a40f949f033
Gregtrnx <146743037+Gregtrnx@users.noreply.github.com>
2024-09-24 16:11:17 +0200

    header + menu

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,21 @@ document.querySelector('.searchbar').addEventListener('focus', function() {
 document.querySelector('.searchbar').addEventListener('blur', function() {
     document.querySelector('.ship').style.display = 'inline';
 });
+
+document.querySelector('.icon').addEventListener('click', function() {
+    this.classList.add('clicked');
+    
+    // Retirer la classe après l'animation pour pouvoir cliquer à nouveau
+    setTimeout(() => {
+        this.classList.remove('clicked');
+    }, 150); // Durée de 150ms pour correspondre à la transition
+});
+
+document.querySelector('form>*').addEventListener('click', function() {
+    this.classList.add('clicked');
+    
+    // Retirer la classe après l'animation pour pouvoir cliquer à nouveau
+    setTimeout(() => {
+        this.classList.remove('clicked');
+    }, 150); // Durée de 150ms pour correspondre à la transition
+});
